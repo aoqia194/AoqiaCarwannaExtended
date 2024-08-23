ff50d747ee8eecbd20e2cf0f4e59983c4819c29d
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-08-23 13:52:44 +1000

    fix: Correctly prevent server entrypoint from executing on the client.

--- src/media/lua/server/AoqiaCarwannaExtendedServer.lua
+++ src/media/lua/server/AoqiaCarwannaExtendedServer.lua
@@ -3,7 +3,7 @@
 -- -------------------------------------------------------------------------- --
 
 -- AoqiaCarwannaExtended requires.
-local constants = require("AoqiaZomboidUtils/constants")
+local constants = require("AoqiaZomboidUtilsShared/constants")
 local events = require("AoqiaCarwannaExtendedServer/events")
 local mod_constants = require("AoqiaCarwannaExtendedShared/mod_constants")
 
@@ -14,11 +14,10 @@ local logger = mod_constants.LOGGER
 
 -- ------------------------------- Entrypoint ------------------------------- --
 
--- Don't load on the client (excluding coop host or singleplayer).
+-- Don't load on the client (excluding singleplayer).
 if  constants.IS_CLIENT
-and constants.IS_SINGLEPLAYER == false
-and constants.IS_COOP == false then
-    logger:debug_server("Prevented server code from being loaded because N O.")
+and constants.IS_SINGLEPLAYER == false then
+    logger:debug("Prevented server entrypoint from being executed because that is bad.")
     return
 end
 
