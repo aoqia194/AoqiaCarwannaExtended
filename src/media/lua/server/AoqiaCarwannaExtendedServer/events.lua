69a6c2806f0adbfe4847bbb82d9eb84505596249
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-08-23 14:03:49 +1000

    fix: Logger call not being passed enough params

--- src/media/lua/server/AoqiaCarwannaExtendedServer/events.lua
+++ src/media/lua/server/AoqiaCarwannaExtendedServer/events.lua
@@ -5,6 +5,7 @@
 -- My modules.
 local blacklists = require("AoqiaCarwannaExtendedServer/blacklists")
 local commands = require("AoqiaCarwannaExtendedServer/commands")
+local constants = require("AoqiaZomboidUtilsShared/constants")
 local mod_constants = require("AoqiaCarwannaExtendedShared/mod_constants")
 
 -- TIS globals.
@@ -19,8 +20,6 @@ local events = {}
 --- @type Callback_OnClientCommand
 function events.on_client_command(module, command, player, args)
     -- This isn't being called when a client tries to send command to server.
-    logger:info_server("RAAAAAAAAAAAAAAAAAH")
-
     if module ~= mod_constants.MOD_ID then return end
     if commands[command] == nil then
         logger:info_server("Received non-existent client command %s.", command)
@@ -32,12 +31,12 @@ function events.on_client_command(module, command, player, args)
         parsedargs = parsedargs .. k .. "=" .. tostring(v) .. ";"
     end
 
-    logger:info_server("Received client command from %s with command %s for player %s.")
+    logger:info_server("Received client command %s from player %s <%s>.", command,
+        player:getUsername(), tostring(player:getSteamID()))
     commands[command](player, args)
 end
 
 --- @type Callback_OnInitGlobalModData
---- @param new_game boolean
 function events.init_global_moddata(new_game)
     blacklists.init()
 end
