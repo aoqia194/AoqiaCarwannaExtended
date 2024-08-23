a39a57fbbec10b46a34d0336165935aa48c15ee5
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-08-23 14:02:44 +1000

    chore: Update LuaCATS class/struct definitions

--- src/media/lua/shared/AoqiaCarwannaExtendedShared/mod_constants.lua
+++ src/media/lua/shared/AoqiaCarwannaExtendedShared/mod_constants.lua
@@ -2,7 +2,7 @@
 --                   Stores constants to be used everywhere.                  --
 -- -------------------------------------------------------------------------- --
 
-local logger = require("AoqiaZomboidUtils/logger")
+local logger = require("AoqiaZomboidUtilsShared/logger")
 
 -- ------------------------------ Module Start ------------------------------ --
 
@@ -17,36 +17,42 @@ mod_constants.FUELTANK_NAMES = { "500FuelTank", "1000FuelTank" }
 
 -- ---------------------------- Class Definitions --------------------------- --
 
---- @class ModDataDummy
---- @field Battery unknown
---- @field Blood {}
---- @field BloodF unknown
---- @field BloodB unknown
---- @field BloodL unknown
---- @field BloodR unknown
---- @field Broken unknown
+--- @class (exact) PartIdDummy
+--- @field Condition integer | int
+--- @field Content float
+--- @field Delta float
+--- @field Item string
+--- @field ModData table<any, any>
+--- @field Model unknown
+mod_constants.PARTID_DUMMY = {}
+
+--- @class (exact) ModDataDummy
+--- @field Battery integer | int
+--- @field Blood { F: float, B: float, L: float, R: float}
 --- @field Clear unknown
 --- @field Color { H: float, S: float, V: float}
---- @field ColorH float
---- @field ColorS float
---- @field ColorV float
---- @field Condition integer
+--- @field Condition integer | int
 --- @field Dir IsoDirections
---- @field EngineQuality unknown
---- @field FuelTank integer
---- @field GasTank unknown
+--- @field EngineLoudness integer | int
+--- @field EnginePower integer | int
+--- @field EngineQuality integer | int
+--- @field FuelTank integer | int
+--- @field GasTank integer | int
 --- @field HasKey boolean
+--- @field HeadlightsActive boolean
+--- @field HeaterActive boolean
 --- @field Hotwire boolean
 --- @field IsBlacklisted boolean
+--- @field LockedDoor boolean
+--- @field LockedTrunk boolean
 --- @field LootChance integer
---- @field MakeKey boolean
---- @field Missing unknown
---- @field OtherTank unknown
---- @field Parts unknown
---- @field Rust unknown
---- @field Skin unknown
+--- @field OtherTank integer | int
+--- @field Parts { [string]: PartIdDummy }
+--- @field PartsBroken integer | int
+--- @field PartsMissing integer | int
+--- @field Rust float
+--- @field Skin integer | int
 --- @field TirePsi integer
---- @field Type string
 --- @field Upgrade boolean
 --- @field VehicleId string
 --- @field VehicleName string
@@ -59,11 +65,13 @@ mod_constants.MOD_DATA_DUMMY = {}
 --- @field DoFixHiddenParts boolean
 --- @field DoFormLoot boolean
 --- @field DoIgnoreHiddenParts boolean
+--- @field DoKeepForm boolean
 --- @field DoLootTables boolean
 --- @field DoRegistration boolean
 --- @field DoRequiresAllParts boolean
 --- @field DoRequiresForm boolean
 --- @field DoRequiresKey boolean
+--- @field DoSafehouseOnly boolean
 --- @field DoShowAllParts boolean
 --- @field DoZedLoot boolean
 --- @field FormLootChance float
@@ -71,6 +79,7 @@ mod_constants.MOD_DATA_DUMMY = {}
 --- @field LootChance float
 --- @field MinimumCondition integer
 --- @field PartWhitelist string
+--- @field SafehouseDistance integer | int
 --- @field TrailerBlacklist string
 --- @field VehicleBlacklist string
 --- @field ZedLootChance float
@@ -84,11 +93,13 @@ mod_constants.SANDBOX_VARS_DUMMY = {
     DoFixHiddenParts = false,
     DoFormLoot = true,
     DoIgnoreHiddenParts = true,
+    DoKeepForm = false,
     DoLootTables = true,
     DoRegistration = true,
     DoRequiresAllParts = true,
     DoRequiresForm = true,
     DoRequiresKey = true,
+    DoSafehouseOnly = false,
     DoShowAllParts = false,
     DoZedLoot = true,
     FormLootChance = 1.0,
@@ -96,6 +107,7 @@ mod_constants.SANDBOX_VARS_DUMMY = {
     LootChance = 1.0,
     MinimumCondition = 100,
     PartWhitelist = "",
+    SafehouseDistance = 10,
     TrailerBlacklist = "",
     VehicleBlacklist = "",
     ZedLootChance = 0.01,
